f24092ef7d676c66e9749d24ba70dff2d4f5f5bb
HexMakina <20891495+HexMakina@users.noreply.github.com>
2025-05-24 21:10:45 +0200

    fix: update HTML language attribute to French and change document title to Copro.Academy

--- io/views/admin/layout.php
+++ io/views/admin/layout.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Copro.Academy</title>
     <link rel="stylesheet" href="/assets/css/admin.css">
     <style>
         body {
