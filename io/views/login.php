8f4341dcdc42fb62189f1eb57f894cd6a347f64e
HexMakina <20891495+HexMakina@users.noreply.github.com>
2025-05-31 18:43:19 +0200

    crsf field

--- io/views/login.php
+++ io/views/login.php
@@ -1,27 +1,27 @@
+<?php if (isset($error)): ?>
+    <div class="alert alert-error">
+        <?= htmlspecialchars($error) ?>
+    </div>
+<?php endif; ?>
 
-            <?php if (isset($error)): ?>
-                <div class="alert alert-error">
-                    <?= htmlspecialchars($error) ?>
-                </div>
-            <?php endif; ?>
+<form action="/login<?= !empty($redirect) ? '?redirect=' . htmlspecialchars($redirect) : '' ?>" method="post" class="auth-form">
+    <?= csrf_field() ?>
+    <div class="form-group">
+        <label for="username" class="form-label">Username</label>
+        <input type="text" id="username" name="username" class="form-control" required autofocus>
+    </div>
 
-            <form action="/login<?= !empty($redirect) ? '?redirect=' . htmlspecialchars($redirect) : '' ?>" method="post" class="auth-form">
-                <div class="form-group">
-                    <label for="username" class="form-label">Username</label>
-                    <input type="text" id="username" name="username" class="form-control" required autofocus>
-                </div>
+    <div class="form-group">
+        <label for="password" class="form-label">Password</label>
+        <input type="password" id="password" name="password" class="form-control" required>
+    </div>
 
-                <div class="form-group">
-                    <label for="password" class="form-label">Password</label>
-                    <input type="password" id="password" name="password" class="form-control" required>
-                </div>
+    <div class="form-check">
+        <input type="checkbox" id="remember" name="remember" class="form-check-input">
+        <label for="remember" class="form-check-label">Remember me</label>
+    </div>
 
-                <div class="form-check">
-                    <input type="checkbox" id="remember" name="remember" class="form-check-input">
-                    <label for="remember" class="form-check-label">Remember me</label>
-                </div>
-
-                <div class="form-actions">
-                    <button type="submit" class="btn btn-primary btn-block">Login</button>
-                </div>
-            </form>
+    <div class="form-actions">
+        <button type="submit" class="btn btn-primary btn-block">Login</button>
+    </div>
+</form>
