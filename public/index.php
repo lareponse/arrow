271272d51e10a0aee3736ff7cb44ed2a25c55fa2
HexMakina <20891495+HexMakina@users.noreply.github.com>
2025-05-31 18:43:58 +0200

    removed pdo and request call, just route, handle, respond

--- public/index.php
+++ public/index.php
@@ -2,18 +2,12 @@
 
 set_include_path(get_include_path() . PATH_SEPARATOR . __DIR__ . '/../..');
 
-
 require 'add/core.php';
 require 'add/bad/error.php';
 require 'add/bad/db.php';
 require 'add/bad/ui.php';
 require 'add/bad/guard_auth.php';
 
-
-
-pdo(...(require 'app/data/credentials.php'));
-
-$_ = request(__DIR__ . '/../io/route');
-$route = route();
+$route = route(__DIR__ . '/../io/route');
 $response = handle($route);
 respond($response);
