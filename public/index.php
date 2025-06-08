5929ff6e3276730c62e1b28cada3a07b2ff27fb5
HexMakina <20891495+HexMakina@users.noreply.github.com>
2025-06-08 16:49:53 +0200

    rendering closure for io() + no db

--- public/index.php
+++ public/index.php
@@ -2,23 +2,30 @@
 
 set_include_path(get_include_path() . PATH_SEPARATOR . __DIR__ . '/../..');
 
-
-require 'add/core.php';
 require 'add/bad/error.php';
-require 'add/bad/db.php';
-require 'add/bad/ui.php';
-require 'add/bad/guard_auth.php';
+require 'add/bad/http.php';
+require 'add/bad/io.php';
+
+require 'add/dad/html.php';
+require 'add/dad/guard_auth.php';
 
 http_guard();
 
-$from      = io(__DIR__ . '/../io/route');
-$map       = io_look($from);
-$quest     = io_walk(io_read($map));
-$response  = deliver($quest) ?: http_response(404, "Not Found", ['Content-Type' => 'text/plain']);
+$origin = realpath(__DIR__ . '/../io');
+$in_path = $origin . '/route';
+$out_path = $origin . '/render';
+$quest = io($in_path, $out_path, function($quest) use ($out_path){
+    slot('main',$quest[IO_SEND | IO_LOAD]);
+    $layout = io_probe($out_path, io_draft(io_clean($_SERVER['REQUEST_URI']), 'layout'));
+    http_respond(200, $layout[IO_LOAD], ['Content-Type' => 'text/html; charset=UTF-8']);
+});
 
-if (is_dev() && empty($response['status']) || $response['status'] >= 400) {
-    $response = http_response(404, io_scaffold('in'), ['Content-Type' => 'text/html; charset=UTF-8']);
+if (is_dev() && empty($response) || $response >= 400) {
+    ob_start();
+    @include 'add/dad/scaffold.php';
+    $scaffold   = trim(ob_get_clean()); // trim helps return ?: null (no opinion, significant whitespaces are in tags)
+    $response = http_response(404, $scaffold, ['Content-Type' => 'text/html; charset=UTF-8']);
 }
 
-http_echo(...$response);
+http_respond(...$response);
 exit;
