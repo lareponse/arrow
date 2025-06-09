949af20ef1823e7509285d2e5c60f5dc77248b5a
HexMakina <20891495+HexMakina@users.noreply.github.com>
2025-06-09 04:46:53 +0200

    refactor: update authentication and improve request handling in index.php

--- public/index.php
+++ public/index.php
@@ -5,19 +5,20 @@ set_include_path(get_include_path() . PATH_SEPARATOR . __DIR__ . '/../..');
 require 'add/bad/error.php';
 require 'add/bad/http.php';
 require 'add/bad/io.php';
+require 'add/bad/dad/html.php';
 
-require 'add/bad/dad/guard_auth.php';
+require 'add/bad/dad/auth.php';
 
 http_guard();
 
 $origin = realpath(__DIR__ . '/../io');
 $in_path = $origin . '/route';
 $out_path = $origin . '/render';
-$quest = io($in_path, $out_path, function($quest) use ($out_path){
-    tray('main',$quest[IO_SEND | IO_LOAD]);
-    $layout = io_probe($out_path, io_draft(io_clean($_SERVER['REQUEST_URI']), 'layout'));
-    http_respond(200, $layout[IO_LOAD], ['Content-Type' => 'text/html; charset=UTF-8']);
-});
+$quest = io($in_path, $out_path);
+tray('main', $quest[IO_SEND | IO_LOAD]);
+$layout = io_probe($out_path, io_draft(io_clean($_SERVER['REQUEST_URI']), 'layout'));
+http_respond(200, $layout[IO_LOAD], ['Content-Type' => 'text/html; charset=UTF-8']);
+
 
 if (is_dev() && empty($response) || $response >= 400) {
     ob_start();
