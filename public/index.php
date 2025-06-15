18c54d9b4d0a44718695d32888ba4a318cbca2ee
HexMakina <20891495+HexMakina@users.noreply.github.com>
2025-06-15 15:22:33 +0200

    update index.php to improve path handling and error management

--- public/index.php
+++ public/index.php
@@ -2,23 +2,23 @@
 
 set_include_path(get_include_path() . PATH_SEPARATOR . __DIR__ . '/../..');
 
-require 'add/bad/dev.php';
+require 'add/bad/dad/dev.php';
 require 'add/bad/error.php';
 require 'add/bad/io.php';
-require 'add/bad/dad/auth.php';
+require 'add/bad/auth.php';
 
-define('BASE', realpath(__DIR__ . '/../io'));
-define('PATH', io_guard(http_guard(4096, 9)));
+define('HOME_BASE', realpath(__DIR__ . '/../io'));
+define('SAFE_PATH', io_guard(http_guard(4096, 9)));
+define('FILE_ROOT', 'index');
 
-$continue   = in(BASE . '/route', PATH, 'index');
+$continue   = io_start(HOME_BASE . '/route', SAFE_PATH, FILE_ROOT);
+$mirror     = io_start(HOME_BASE . '/render/', SAFE_PATH, FILE_ROOT, $continue);
+vd(0, $mirror);die;
 
-// no http() call made in the route, we continue with custom handling
-$mirror     = BASE . '/render/' . $continue[IO_PATH];
-$mirror     = ob_capture($mirror, $continue[IO_ARGS] ?: []);
-
+$mirror     = io_absorb($mirror[IO_PATH], $continue[IO_ARGS] ?: []);
 if (is_array($mirror)) {
     http(...$mirror);
     exit;
 }
 
-throw new RuntimeException('Not Found', 404);
+http(404, 'Not Found');
