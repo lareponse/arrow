454c0122adb7e160ff290ee396f9f11be8ab6840
HexMakina <20891495+HexMakina@users.noreply.github.com>
2025-06-22 23:08:16 +0200

    feat: add new SQL views for articles and training with category and trainer details

--- data/004-struct-taxonomy-views.sql
+++ data/004-struct-taxonomy-views.sql
@@ -14,3 +14,26 @@ SELECT
 FROM taxonomy AS c
 JOIN taxonomy AS p ON c.parent_id = p.id
 WHERE c.revoked_at IS NULL;
+
+DROP VIEW IF EXISTS article_with_cateogry;
+CREATE VIEW article_with_cateogry AS
+SELECT
+  a.*,
+  c.slug      AS category_slug,
+  c.label     AS category_label
+FROM article AS a
+JOIN taxonomy_with_parent AS c ON a.category_id = c.id
+WHERE a.revoked_at IS NULL;
+
+DROP VIEW IF EXISTS training_with_level_trainer;
+CREATE VIEW training_with_level_trainer AS
+SELECT
+  training.*,
+  l.slug      AS level_slug,
+  l.label     AS level_label,
+  trainer.slug      AS trainer_slug,
+  trainer.label     AS trainer_label
+FROM training
+JOIN taxonomy_with_parent AS l ON training.level_id = l.id
+JOIN trainer ON training.trainer_id = trainer.id
+WHERE training.revoked_at IS NULL;
