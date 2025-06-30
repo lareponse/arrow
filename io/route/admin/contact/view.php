39f03ab2eaff9d1834d4e49031192b5d4f1a7c7d
HexMakina <20891495+HexMakina@users.noreply.github.com>
2025-06-30 15:55:16 +0200

    fix: improve redirect logic for missing contact requests in admin view

--- io/route/admin/contact/view.php
+++ io/route/admin/contact/view.php
@@ -6,7 +6,7 @@ return function ($args) {
     auth(AUTH_GUARD);
 
     $contact = row(db(), 'contact_request_plus')(ROW_LOAD, ['id' => array_pop($args)]);
-    !$contact && http_back() && exit;
+    !$contact && header('Location: ' . ($_SERVER['HTTP_REFERER'] ?? '/')) && exit;
 
     return ['contact' => $contact];
 };
