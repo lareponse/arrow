0ac4b7545032d7e82fe71a1de71ba1dd8bbf02a5
HexMakina <20891495+HexMakina@users.noreply.github.com>
2025-06-30 22:05:46 +0200

    feat: simplify booking statistics calculation in admin dashboard

--- io/route/admin/admin.php
+++ io/route/admin/admin.php
@@ -1,7 +1,6 @@
 <?php
 
 return function () {
-    auth(AUTH_GUARD);
     return [
         'title' => 'Tableau de bord - Copro Academy Admin',
         'stats' => stats(),
@@ -28,29 +27,9 @@ function stats()
         'trainings_change' => (int)dbq(db(), sprintf($changes, 'training'))->fetchColumn(),
         'events_total' => (int)dbq(db(), "SELECT COUNT(*) FROM event WHERE revoked_at IS NULL")->fetchColumn(),
         'events_change' => (int)dbq(db(), sprintf($changes, 'event'))->fetchColumn(),
-
-        'bookings_total' => (int)dbq(db(), "
-            SELECT COUNT(*) FROM event_booking eb 
-            JOIN event e ON eb.event_id = e.id 
-            WHERE eb.revoked_at IS NULL AND e.revoked_at IS NULL
-        ")->fetchColumn() + (int)dbq(db(), "
-            SELECT COUNT(*) FROM training_booking tb 
-            JOIN training t ON tb.training_id = t.id 
-            WHERE tb.revoked_at IS NULL AND t.revoked_at IS NULL
-        ")->fetchColumn(),
+        'booking_total' => (int)dbq(db(), "SELECT COUNT(*) FROM booking eb  WHERE eb.revoked_at IS NULL ")->fetchColumn(),
+        'bookings_change' => (int)dbq(db(), "SELECT COUNT(*) FROM booking tb WHERE tb.created_at >= DATE_SUB(CURRENT_DATE, INTERVAL 1 MONTH) AND tb.revoked_at IS NULL ")->fetchColumn()
     ];
-
-
-    $stats['bookings_total'] =
-        (int)dbq(db(), "SELECT COUNT(*) FROM event_booking eb JOIN event e ON eb.event_id = e.id WHERE eb.revoked_at IS NULL AND e.revoked_at IS NULL")->fetchColumn()
-        + (int)dbq(db(), "SELECT COUNT(*) FROM training_booking tb JOIN training t ON tb.training_id = t.id WHERE tb.revoked_at IS NULL AND t.revoked_at IS NULL")->fetchColumn();  // Initialize bookings change
-
-    // Monthly changes
-
-
-    $stats['bookings_change'] =
-        (int)dbq(db(), "SELECT COUNT(*) FROM event_booking eb JOIN event e ON eb.event_id = e.id WHERE eb.created_at >= DATE_SUB(CURRENT_DATE, INTERVAL 1 MONTH) AND eb.revoked_at IS NULL AND e.revoked_at IS NULL")->fetchColumn()
-        + (int)dbq(db(), "SELECT COUNT(*) FROM training_booking tb JOIN training t ON tb.training_id = t.id WHERE tb.created_at >= DATE_SUB(CURRENT_DATE, INTERVAL 1 MONTH) AND tb.revoked_at IS NULL AND t.revoked_at IS NULL")->fetchColumn();
 
     return $stats;
 }
