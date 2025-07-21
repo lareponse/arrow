f2fe7b77dd1a999fb8bcbc41f157fbf6a334ea8b
HexMakina <20891495+HexMakina@users.noreply.github.com>
2025-07-22 01:00:16 +0200

    streamline stats function and optimize database queries

--- io/route/admin/admin.php
+++ io/route/admin/admin.php
@@ -5,31 +5,20 @@ return function () {
         'title' => 'Tableau de bord - Copro Academy Admin',
         'stats' => stats(),
         'recent' => [
-            'contacts' => dbq(db(), "SELECT * FROM contact_request_plus LIMIT 5")->fetchAll(),
-            'events' => dbq(db(), "SELECT * FROM event_plus WHERE event_date >= CURRENT_DATE LIMIT 5")->fetchAll(),
+            'contacts'  => db()->query("SELECT * FROM contact_request_plus LIMIT 5")->fetchAll(),
+            'events'    => db()->query("SELECT * FROM event_plus WHERE event_date >= CURRENT_DATE LIMIT 5")->fetchAll(),
         ],
     ];
 };
 
 function stats()
 {
+    $change = 'SELECT COUNT(*) FROM `%s` WHERE `created_at` >= DATE_SUB(CURRENT_DATE, INTERVAL 1 MONTH) AND `revoked_at` IS NULL';
+    $total = 'SELECT COUNT(*) FROM `%s` WHERE revoked_at IS NULL';
 
-    // Monthly changes
-    $changes = 'SELECT COUNT(*) FROM `%s` 
-        WHERE `created_at` >= DATE_SUB(CURRENT_DATE, INTERVAL 1 MONTH)
-        AND `revoked_at` IS NULL';
-
-
-    $stats = [
-        'articles_total' => (int)dbq(db(), "SELECT COUNT(*) FROM article WHERE revoked_at IS NULL")->fetchColumn(),
-        'articles_change' => (int)dbq(db(), sprintf($changes, 'article'))->fetchColumn(),
-        'trainings_total' => (int)dbq(db(), "SELECT COUNT(*) FROM training WHERE revoked_at IS NULL")->fetchColumn(),
-        'trainings_change' => (int)dbq(db(), sprintf($changes, 'training'))->fetchColumn(),
-        'events_total' => (int)dbq(db(), "SELECT COUNT(*) FROM event WHERE revoked_at IS NULL")->fetchColumn(),
-        'events_change' => (int)dbq(db(), sprintf($changes, 'event'))->fetchColumn(),
-        'booking_total' => (int)dbq(db(), "SELECT COUNT(*) FROM booking eb  WHERE eb.revoked_at IS NULL ")->fetchColumn(),
-        'bookings_change' => (int)dbq(db(), "SELECT COUNT(*) FROM booking tb WHERE tb.created_at >= DATE_SUB(CURRENT_DATE, INTERVAL 1 MONTH) AND tb.revoked_at IS NULL ")->fetchColumn()
-    ];
-
+    foreach(['total', 'change'] as $type)
+        foreach(['article', 'training', 'event', 'booking'] as $table) 
+            $stats["{$table}s_{$type}"] = (int)db()->query(sprintf($$type, $table))->fetchColumn();
+        
     return $stats;
 }
